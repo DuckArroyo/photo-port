ec5333deed70d5cbd352783b48ddede7a6605ba0
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-12-11 19:20:02 -0800

    outlined nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Nav from "./components/Nav";
 import About from "./components/about";
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
