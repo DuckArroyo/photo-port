494ddf32c4b5fcfaf3fc1a044a905b1c9ae6251c
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-12-12 17:10:34 -0800

    front end form handling functionality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,32 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
   //JSX
-  <section>
-    <h1>Contact Me</h1>
 
-    <form id="contact-form">
-      {/* name input */}
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" name="name" />
-      </div>
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-      {/* email input */}
-      <div>
-        <label htmlFor="email">Email address:</label>
-        <input type="email" name="email" />
-      </div>
+  //destructuring formState into named properties
+  const { name, email, message } = formState;
 
-      {/* message text area */}
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea name="message" rows="5" />
-      </div>
+  //handles user input inside the form, for all form elements.
+  function handleChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
 
-      <button type="submit">Submit</button>
-    </form>
-  </section>;
+  //console.log(formState); //console logs as user enters text on contact form
+
+  //Submit the Form data function
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
+  return (
+    <section>
+      <h1>Contact Me</h1>
+
+      <form id="contact-form" onSubmit={handleSubmit}>
+        {/* name input */}
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            defaultValue={name}
+            name="name"
+            onChange={handleChange}
+          />
+        </div>
+
+        {/* email input */}
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="email"
+            defaultValue={email}
+            name="email"
+            onChange={handleChange}
+          />
+        </div>
+
+        {/* message text area */}
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="message"
+            defaultValue={message}
+            rows="5"
+            onChange={handleChange}
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
 }
 
 export default ContactForm;
